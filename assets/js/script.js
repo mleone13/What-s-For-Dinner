ef94104e65f41e921414cafea7339e904e74eae8
EMartinezCastellanos <97920151+EMartinezCastellanos@users.noreply.github.com>
2022-05-16 00:29:47 -0700

    successfully fetched drinks and its name and ingredients, need to find a way to implement more than 1 ingredient at a time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,39 @@
 var cocktailContainer = document.getElementById('whole-drink');
 var drinkName = document.getElementById('d-name');
 var ingredients = document.getElementById('ingredients');
-var measurements = document.getElementById('measure');
 var directions = document.getElementById('how');
-// fetch(
-//     'https://thecocktaildb.com/api/json/v1/1/random.php'
-// )
-//     .then(function(response) {
-//         return response.json();
-//     })
-//     .then(function(data) {
-//         console.log(data);
-//         for (var i=0; i < data.length; i++){
-//             var drinkName = document.createElement('h1');
-//             var ingredients = document.createElement('h3');
-//             var instructions = document.createElement('h3');
+var drinkPic = document.getElementById('gram-pic');
 
-//             drinkName.textContent = data[i].strDrink;
-//             ingredients.textContent = data[i].strIngredient1;
-//             instructions.textContent = data[i].strInstructions;
-
-//             cocktailContainer.append(drinkName);
-//             cocktailContainer.append(ingredients);
-//             cocktailContainer.append (instructions);
-//         }
-//     });
-
-// function getCocktail (){
-//     var cocktailAPI = 'https://thecocktaildb.com/api/json/v1/1/random.php';
-//     fetch(cocktailAPI)
-//     .then(function (response) {
-//         return response.json();
-//     })
-//     .then(function(data) {
-//         console.log(data);
-
-//             drinkName.textContent = data[].strDrink;
-//             ingredients.textContent = data[].strIngredient1;
-//             directions.textContent = data[].strInstructions;
-//     });
-// }  
-
-// getCocktail();
 fetch('https://thecocktaildb.com/api/json/v1/1/random.php')
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
+    //    debug to make sure feth request was successful
         console.log(data);
         console.log(data.array);
-        for (var i=0; i < data.length; i++){
-            console.log(data[i].drink);
-            console.log(data[i].strDrink)
-        }
+        for (var i=0; i < data.drinks.length; i++){
+            // debug tool to see if for loop is generating desired variables
+            console.log(data.drinks[i].drink);
+             console.log(data.drinks[i].strDrink)
+             console.log(data.drinks[i].strGlass)
+
+            var cName = document.createElement('h1');
+            var cIng = document.createElement('li');
+            var cDir = document.createElement('p');
+            var cPic = document.createElement('img');
+
+
+            cName.textContent = data.drinks[i].strDrink;
+            cIng.textContent =data.drinks[i].strIngredient1 + ' ' +  data.drinks[i].strMeasure1;
+            cDir.textContent= data.drinks[i].strInstructions;
+            cPic.link= data.drinks[i].strDrinkThumb;
+            
+
+            drinkName.append(cName);
+            ingredients.append(cIng);
+            directions.append(cDir);
+            drinkPic.append(cPic);
+           }
     });
 
