06b0f1b2132a5cd594deb21ee8b7e08065504e46
EMartinezCastellanos <97920151+EMartinezCastellanos@users.noreply.github.com>
2022-05-16 01:03:56 -0700

    added ability to generate image of generated drink

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,18 +20,52 @@ fetch('https://thecocktaildb.com/api/json/v1/1/random.php')
 
             var cName = document.createElement('h1');
             var cIng = document.createElement('li');
+            var cIng2 = document.createElement('li');
+            var cIng3= document.createElement('li');
+            var cIng4 = document.createElement('li');
+            var cIng5 = document.createElement('li');
+            var cIng6 = document.createElement('li');
+            var cIng7 = document.createElement('li');
+            var cIng8 = document.createElement('li');
             var cDir = document.createElement('p');
             var cPic = document.createElement('img');
 
 
             cName.textContent = data.drinks[i].strDrink;
             cIng.textContent =data.drinks[i].strIngredient1 + ' ' +  data.drinks[i].strMeasure1;
+            cIng2.textContent =data.drinks[i].strIngredient2 + ' ' +  data.drinks[i].strMeasure2;
+            cIng3.textContent =data.drinks[i].strIngredient3 + ' ' +  data.drinks[i].strMeasure3;
+            cIng4.textContent =data.drinks[i].strIngredient4 + ' ' +  data.drinks[i].strMeasure4;
+            cIng5.textContent =data.drinks[i].strIngredient5 + ' ' +  data.drinks[i].strMeasure5;
+            cIng6.textContent =data.drinks[i].strIngredient6 + ' ' +  data.drinks[i].strMeasure6;
+            cIng7.textContent =data.drinks[i].strIngredient7 + ' ' +  data.drinks[i].strMeasure7;
             cDir.textContent= data.drinks[i].strInstructions;
-            cPic.link= data.drinks[i].strDrinkThumb;
-            
+            cPic.src= data.drinks[i].strDrinkThumb;
+
+            if (cIng2.textContent=('null')){
+                cIng2.textContent='';
+            }
+            if (cIng3.textContent=('null')){
+                cIng3.textContent='';
+            }
+            if (cIng4.textContent=('null')){
+                cIng4.textContent='';
+            }
+            if (cIng5.textContent=('null')){
+                cIng5.textContent='';
+            }
+            if (cIng6.textContent=('null')){
+                cIng6.textContent='';
+            }
+            if (cIng7.textContent=('null')){
+                cIng7.textContent='';
+            }
+            if (cIng8.textContent=('null')){
+                cIng8.textContent='';
+            }
 
             drinkName.append(cName);
-            ingredients.append(cIng);
+            ingredients.append(cIng,cIng2,cIng3,cIng4,cIng5,cIng6,cIng7);
             directions.append(cDir);
             drinkPic.append(cPic);
            }
