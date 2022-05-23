c8dee6f4cfb75536a8fa46e9469cd3b1254af575
EMartinezCastellanos <97920151+EMartinezCastellanos@users.noreply.github.com>
2022-05-22 19:02:27 -0700

    added functionality to replace cocktail generated with a new one upon button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,28 @@
-var drinkName =document.getElementById('cocktail-name');
+var drinkName = document.getElementById('cocktail-name');
 var ingredients = document.getElementById('cocktail-ingredients');
 var drinkDirections = document.getElementById('cocktail-directions');
 var drinkPic = document.getElementById('cocktail-image');
-
-
-
-
+function fetchCocktail(){
 fetch('https://thecocktaildb.com/api/json/v1/1/random.php')
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-    //    debug to make sure feth request was successful
+        //    debug to make sure feth request was successful
         console.log(data);
         // console.log(data.array);
-        for (var i=0; i < data.drinks.length; i++){
+        for (var i = 0; i < data.drinks.length; i++) {
             // debug tool to see if for loop is generating desired variables
             // console.log(data.drinks[i].drinks);
-             console.log(data.drinks[i].strDrink)
-             console.log(data.drinks[i].strGlass)
-             console.log(data.drinks[i].strIngredient1);
-             console.log(data.drinks[i].strInstructions);
+            console.log(data.drinks[i].strDrink)
+            console.log(data.drinks[i].strGlass)
+            console.log(data.drinks[i].strIngredient1);
+            console.log(data.drinks[i].strInstructions);
 
             var cName = document.createElement('h1');
             var cIng = document.createElement('li');
             var cIng2 = document.createElement('li');
-            var cIng3= document.createElement('li');
+            var cIng3 = document.createElement('li');
             var cIng4 = document.createElement('li');
             var cIng5 = document.createElement('li');
             var cIng6 = document.createElement('li');
@@ -35,15 +32,15 @@ fetch('https://thecocktaildb.com/api/json/v1/1/random.php')
 
 
             cName.textContent = data.drinks[i].strDrink;
-            cIng.textContent =data.drinks[i].strIngredient1 + ' ' +  data.drinks[i].strMeasure1;
-            cIng2.textContent =data.drinks[i].strIngredient2 + ' ' +  data.drinks[i].strMeasure2;
-            cIng3.textContent =data.drinks[i].strIngredient3 + ' ' +  data.drinks[i].strMeasure3;
-            cIng4.textContent =data.drinks[i].strIngredient4 + ' ' +  data.drinks[i].strMeasure4;
-            cIng5.textContent =data.drinks[i].strIngredient5 + ' ' +  data.drinks[i].strMeasure5;
-            cIng6.textContent =data.drinks[i].strIngredient6 + ' ' +  data.drinks[i].strMeasure6;
-            cIng7.textContent =data.drinks[i].strIngredient7 + ' ' +  data.drinks[i].strMeasure7;
-            cDir.textContent= data.drinks[i].strInstructions;
-            cPic.src= data.drinks[i].strDrinkThumb;
+            cIng.textContent = data.drinks[i].strIngredient1 + ' ' + data.drinks[i].strMeasure1;
+            cIng2.textContent = data.drinks[i].strIngredient2 + ' ' + data.drinks[i].strMeasure2;
+            cIng3.textContent = data.drinks[i].strIngredient3 + ' ' + data.drinks[i].strMeasure3;
+            cIng4.textContent = data.drinks[i].strIngredient4 + ' ' + data.drinks[i].strMeasure4;
+            cIng5.textContent = data.drinks[i].strIngredient5 + ' ' + data.drinks[i].strMeasure5;
+            cIng6.textContent = data.drinks[i].strIngredient6 + ' ' + data.drinks[i].strMeasure6;
+            cIng7.textContent = data.drinks[i].strIngredient7 + ' ' + data.drinks[i].strMeasure7;
+            cDir.textContent = data.drinks[i].strInstructions;
+            cPic.src = data.drinks[i].strDrinkThumb;
 
             // if (cIng2.textContent=('null')){
             //     cIng2.textContent='';
@@ -68,11 +65,35 @@ fetch('https://thecocktaildb.com/api/json/v1/1/random.php')
             // }
 
             drinkName.append(cName);
-            ingredients.append(cIng,cIng2,cIng3,cIng4,cIng5,cIng6,cIng7);
+            ingredients.append(cIng, cIng2, cIng3, cIng4, cIng5, cIng6, cIng7);
             drinkDirections.append(cDir);
             drinkPic.appendChild(cPic);
-           }
+        }
     });
+}
+
+var button = document.getElementById('button');
+button.onclick = function () {
+    // button debugging
+    console.log("it's working");
+
+    fetchCocktail();
+    while (drinkName.hasChildNodes()){
+        drinkName.removeChild(drinkName.firstChild);
+    }
+    while (ingredients.hasChildNodes()){
+        ingredients.removeChild(ingredients.firstChild);
+    }
+    while (drinkDirections.hasChildNodes()){
+        drinkDirections.removeChild(drinkDirections.firstChild);
+    }
+    while (drinkPic.hasChildNodes()){
+        drinkPic.removeChild(drinkPic.firstChild);
+    }
+    
+}
+
+
 
 
 
